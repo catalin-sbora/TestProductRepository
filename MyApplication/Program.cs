a2609af6151202769016387257e33269ae1e6077
Catalin Sbora <catalin.sbora@softelligence.net>
2024-03-26 11:43:15 +0200

    bad app

--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This is my first console application");
+﻿Consasdasdasdole.WriteLine("This is my first console application");
+
